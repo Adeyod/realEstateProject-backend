0b4c11f2bd45a7ac71b569d7d16a62ccf158c9f7
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2023-12-25 04:41:43 +0100

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 
 const port = process.env.PORT || 1111;
+
+// connecting the server
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
